5d96a987c642205f83fa5abe7cf381338760a9dc
Denyse <mdenyse15@gmail.com>
2022-01-14 18:02:17 +0200

    Feat:(Added testing if you are on the right page)

---
+++ cypress/integration/my-own-tests/testing-urls.js
@@ -0,0 +1,13 @@
+/// <reference types="cypress" />
+
+describe("Test urls", () => {
+  it("Testing if page contains forgot password", () => {
+    // cy.viewport(516, 561);
+    cy.viewport("macbook-16");
+    cy.visit("https://groovin.vercel.app/");
+    cy.contains("Login").click();
+    cy.contains("Forgot password").click();
+    cy.url().should("include", "forgotpassword");
+    cy.url().should("eq", "https://groovin.vercel.app/forgotpassword");
+  });
+});
