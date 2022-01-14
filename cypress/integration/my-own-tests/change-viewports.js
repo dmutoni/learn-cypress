2446f17cc1d908c8e048e39cf1a4d26e172e41e0
Denyse <mdenyse15@gmail.com>
2022-01-14 16:56:32 +0200

    Feat: (Added changing to a specific device when testing)

--- cypress/integration/my-own-tests/change-viewports.js
+++ cypress/integration/my-own-tests/change-viewports.js
@@ -2,7 +2,8 @@
 
 describe("Test changing viewport", () => {
   it("Change viewport of mineduc website", () => {
-    cy.viewport(516, 561);
+    // cy.viewport(516, 561);
+    cy.viewport("iphone-8");
     cy.visit("https://www.mineduc.gov.rw");
   });
 });
