11ea84a61db039d3dd45961bbcf90aadb7cfd8f3
Denyse <mdenyse15@gmail.com>
2022-01-14 19:05:22 +0200

    Feat:(Added testing on login fail)

--- cypress/integration/my-own-tests/typing-into-fields.js
+++ cypress/integration/my-own-tests/typing-into-fields.js
@@ -13,4 +13,17 @@ describe("Test urls", () => {
     cy.get('input[name="password"]').type("admin");
     cy.get("form").submit();
   });
+  it.only("Login shuold not work", () => {
+    // cy.viewport(516, 561);
+    cy.viewport("macbook-16");
+
+    cy.visit("https://groovin.vercel.app/");
+
+    cy.contains("Login").click();
+
+    cy.get('input[name="email"]').type("testing123@gmail.com");
+    cy.get('input[name="password"]').type("admin");
+    cy.get("form").submit();
+    cy.contains("Invalid email or password").should("exist");
+  });
 });
